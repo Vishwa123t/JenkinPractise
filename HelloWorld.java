7a7628611a77c45c3a5302495d632aa11ba10c13
Vishwa123t <vishwanath.mh02@gmail.com>
2024-01-23 11:21:27 +0530

    new print update

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+public static void main(String[] args) {
+
+System.out.println("Hello joki");
+System.out.println("Donno any basics ashole");
+System.out.println("Learning something everyday");
+System.out.println("New features are incoming");
+}
+}
