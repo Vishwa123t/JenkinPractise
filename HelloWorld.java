8eac00e9648af97686c798a09b46f4ad69f52f63
Vishwa123t <vishwanath.mh02@gmail.com>
2024-01-23 11:47:03 +0530

    See the changes2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 public static void main(String[] args) {
 
-System.out.println("Hello joki");
+System.out.println("Hello joking uh");
 System.out.println("Donnoe any basics ass-hole");
 System.out.println("Learning something everyday");
 System.out.println("New features are incoming");
