f61d65700ffd3c0eed1e00a9f05a16842400909a
Vishwa123t <vishwanath.mh02@gmail.com>
2024-01-23 11:44:03 +0530

    New chnage on line 6

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 public static void main(String[] args) {
 
 System.out.println("Hello joki");
-System.out.println("Donno any basics ashole");
+System.out.println("Donnoe any basics ass-hole");
 System.out.println("Learning something everyday");
 System.out.println("New features are incoming");
 }
